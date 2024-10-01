b231da1101aaccd323403978e42ea1d0a7aa5cfb
tayT0T <pwachara@ucsd.edu>
2024-09-30 22:56:03 -0700

    Delete Testing using MPI// fixing ci.yml workflow to support min 1.5, nightly
    Delete Testing using MPI, delete Test_MPI.jl file. Also update the function to export updated! function.
    For ci.yml, fixing matrix version, based on https://discourse.julialang.org/t/easy-workflow-file-for-setting-up-github-actions-ci-for-your-julia-package/49765

--- src/LightMC.jl
+++ src/LightMC.jl
@@ -1,6 +1,6 @@
 module LightMC
 export readparams, interface!, transfer!, convertwave!, applybc!, exported, phasePetzold, 
-        interface, updateed!, applybc!, setwave!, readdata, Param, writeparams
+        interface, updateed!, applybc!, setwave!, readdata, Param, writeparams, updateed!
 
 include("functions.jl")
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,7 @@
 using LightMC
 using Test 
-using YAML
-using MPI
-using UnicodePlots
 
 @testset "LightMC.jl" begin
     @info "testing the functions being used in lightMC.jl package"
     run(`julia test_function.jl`)
-    
-    @info "testing muliple cpu via MPI package"
-    run(`mpirun -n 4 julia test_mpi.jl`)
 end
